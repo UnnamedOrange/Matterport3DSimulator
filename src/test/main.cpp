1c26ce6c73fec463fadcbdf38fe71d490988d436
UnnamedOrange <lycheng1215@sina.com>
2026-01-13 09:59:19 +0800

    Fix compile

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -12,6 +12,7 @@
 #include <json/json.h>
 #include <opencv2/opencv.hpp>
 
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
 #include "Catch.hpp"
 #include "MatterSim.hpp"
 
@@ -376,7 +377,7 @@ TEST_CASE( "RGB Image", "[Rendering]" ) {
             auto imgfile = testcase["reference_image"].asString();
             auto reference_image = cv::imread("webgl_imgs/"+imgfile);
             auto state = sim.getState().at(n);
-            double err = cv::norm(reference_image, state->rgb, CV_L2);
+            double err = cv::norm(reference_image, state->rgb, cv::NORM_L2);
             err /= reference_image.rows * reference_image.cols;
             CHECK(err < 0.15);
             // save for later comparison, these images can also be inspected by hand
